e12718d477180248699f9b6626af8ec03884dbfd
esafaa <132300437+esafaa@users.noreply.github.com>
2023-11-11 10:14:34 -0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,16 @@
-import sys
-
 def fibonacci(n):
     fib_sequence = [0, 1]
     for i in range(2, n):
         fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
     return fib_sequence
 
-def main():
-    nterms = int(sys.argv[1]) if len(sys.argv) > 1 else 5  # Default to 5 if not provided
+def main(nterms):
     result = fibonacci(nterms)
     print(f"Fibonacci sequence up to {nterms} terms: {result}")
 
 if __name__ == "__main__":
-    main()
+    import sys
+    nterms = int(sys.argv[1]) if len(sys.argv) > 1 else 5  # Default to 5 if not provided
+    main(nterms)
 
 
